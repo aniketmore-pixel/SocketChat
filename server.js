c4156eb5254daec40d3d314f51a7719841463aab
aniketmore-pixel <aniketmore.personal@gmail.com>
2025-09-02 15:04:31 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const io = require('socket.io')(4000, {
+    cors: { origin: "*" }
+})
 
-const app = express();
-const server = http.createServer(app);
+const users = {}
 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
-
-const PORT = process.env.PORT || 3000;
-
-const users = {};
-
-io.on("connection", (socket) => {
-  socket.on("new-user", (name) => {
-    users[socket.id] = name;
-    socket.broadcast.emit("user-connected", name);
-  });
-
-  socket.on("send-chat-message", (message) => {
-    socket.broadcast.emit("chat-message", {
-      message: message,
-      name: users[socket.id],
-    });
-  });
-
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("user-disconnected", users[socket.id]);
-    delete users[socket.id];
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
